c4043e1312ec48c06167e3b0ce1c993b2b77b7c1
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-03-28 23:31:50 -0500

    JavaScript Breakdown: Modules, introduction

--- javascript-breakdown.js
+++ javascript-breakdown.js
@@ -1564,9 +1564,32 @@ console.log(title);
 }
 
 /* 6. Modules */
-// Modules encapsulate all sorts of code like functions and variables and expose all this to other files. Generally, we use it to break our code into separate files to make it more maintainable. They were introduced into JavaScript with ECMAScript 6. There are two types of modules in JavaScript: ES Modules and CommonJS modules. ES Modules are the standard for working with modules in JavaScript and are supported in modern browsers and Node.js. CommonJS modules are used in Node.js and provide a way to organize code into separate files. Modules can be imported and exported using the import and export keywords.
 
 {
+  // 0. As our application grows bigger, we want to split it into multiple files, so called “modules”. A module may contain a class or a library of functions for a specific purpose. Modules encapsulate all sorts of code like functions and variables and expose all this to other files. Generally, we use it to break our code into separate files to make it more maintainable. They were introduced into JavaScript with ECMAScript 6. There are two types of modules in JavaScript: ES Modules and CommonJS modules. ES Modules are the standard for working with modules in JavaScript and are supported in modern browsers and Node.js. CommonJS modules are used in Node.js and provide a way to organize code into separate files. Modules can be imported and exported using the import and export keywords.
+
+// A module is just a file. One script is one module. As simple as that.
+
+// Modules can load each other and use special directives export and import to interchange functionality, call functions of one module from another one:
+
+// export: keyword labels variables and functions that should be accessible from outside the current module.
+// import: allows the import of functionality from other modules.
+
+// For instance, if we have a file sayHi.js exporting a function:
+
+// 📁 sayHi.js
+// export function sayHi(user) {
+//   alert(`Hello, ${user}!`);
+// }
+
+
+// Then we can import it in another file:
+// 📁 main.js
+// import {sayHi} from './sayHi.js';
+
+// alert(sayHi); // function...
+// sayHi('John'); // Hello, John!s
+  
   // 1. ES Modules: ES Modules (ECMAScript Modules) are a standard for working with modules in JavaScript. They provide a way to organize and structure code by splitting it into separate files, each containing a module. ES Modules use the import and export keywords to define dependencies between modules and share code between them.
   // ES Modules - Examples:
   // ES Modules - Exporting a module
