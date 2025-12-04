9696307063245b3a1df2ffa79c18a7fb94ec1525
Dario Ernesto Roca <darioernesto.roca@gmail.com>
2025-12-03 23:48:09 -0500

    Typescript breakdown: definition

---
+++ typescript-breakdown.ts
@@ -0,0 +1,11 @@
+/* TYPESCRIPT BREAKDOWN */
+const title = "Typescript Breakdown";
+console.log(title);
+
+/* 1. What is Typescript */
+
+{
+    /* Typescript is a superset of JavaScript that adds static typing and other features to the language. It is developed and maintained by Microsoft. */
+    const definition = "Typescript is a superset of JavaScript that adds static typing and other features.";
+    console.log(definition);
+}
